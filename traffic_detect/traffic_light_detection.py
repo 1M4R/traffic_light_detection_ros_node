c8b673d2ac537c0622f33b107eb8298b6ff3dd85
1M4R <1M4R@github.com>
2024-06-25 16:33:41 +0800

    stop function

--- traffic_detect/traffic_light_detection.py
+++ traffic_detect/traffic_light_detection.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from bboxes_ex_msgs.msg import BoundingBox  # Import your custom message
+from bboxes_ex_msgs.msg import BoundingBoxes, BoundingBox, ObjectCount  # Import your custom message
 import time
 
 class StopRobotNode(Node):
@@ -11,23 +11,36 @@ class StopRobotNode(Node):
     def __init__(self):
         super().__init__('stop_robot_node')
         self.subscription = self.create_subscription(
-            BoundingBox,
-            '/yolov5/bounding_box',
+            BoundingBoxes,
+            'yolov5/bounding_boxes',
             self.listener_callback,
             10)
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.get_logger().info('StopRobotNode has been started.')
 
     def listener_callback(self, msg):
-        object_class = msg.class_
-        if object_class == 'desired_object_type':  # Replace with your specific object type
-            self.get_logger().info(f'Detected object: {object_class}. Stopping robot.')
-            stop_msg = Twist()
-            stop_msg.linear.x = 0.0
-            stop_msg.angular.z = 0.0
-            self.publisher.publish(stop_msg)
-            time.sleep(4)  # Stop for 4 seconds
-            self.get_logger().info('Resuming operation.')
+        
+        bboxes = BoundingBoxes.bounding_boxes
+        msg=BoundingBox()
+       # self.get_logger().info()
+        for i in range(1):
+            msg=bboxes[i]
+            print(msg)
+            dir(msg)
+            object_class = msg.class_id[i]
+
+            print(object_class)
+            self.get_logger().info('im in2')
+            if object_class == 'traffic light':  # Replace with your specific object type
+                self.get_logger().info(f'Detected object: {object_class}. Stopping robot.')
+                stop_msg = Twist()
+                stop_msg.linear.x = 0.0
+                stop_msg.angular.z = 0.0
+                self.publisher.publish(stop_msg)
+                time.sleep(4)  # Stop for 4 seconds
+                self.get_logger().info('Resuming operation.')
+            i+=1
+        return None
 
 def main(args=None):
     rclpy.init(args=args)
